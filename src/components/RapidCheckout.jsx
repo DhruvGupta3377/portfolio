5fe36bac9a35eae324c409fc8ad420bdd94a911c
dhruvgupta3377 <dhruvgupta3377@gmail.com>
2024-02-24 16:17:37 +0530

    ripcheckout

--- src/components/RapidCheckout.jsx
+++ src/components/RapidCheckout.jsx
@@ -17,11 +17,11 @@ const RapidCheckout = () => {
       <ul>
         <li>
           Developed a solution for optimizing the shopping experience in
-          largecomplexes.
+          large shopping complexes.
         </li>
         <li>
           A service that provides the real-time location of items in the
-          complex, facilitating efficient navigation for customers.{" "}
+          complex, facilitating efficient navigation for customers.
         </li>
         <li>
           The platform features a self-checkout service to alleviate congestion
@@ -29,12 +29,13 @@ const RapidCheckout = () => {
         </li>
         <li>
           Customers can use the built-in scanner on the website for
-          automatically identifying products through barcodes.{" "}
+          automatically identifying products through barcode.
         </li>
         <li>
           The admin dashboard enables store owners to manage inventory
           seamlessly.
         </li>
+        <li>JWT tokens are used for easy authorization and authentication</li>
       </ul>
     </div>
   );
